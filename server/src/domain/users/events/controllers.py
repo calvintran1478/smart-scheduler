2961dfd510996025a721a955a79f4bed977bb643
Calvin Tran <calvintran1478@protonmail.com>
2024-05-28 18:09:02 -0400

    Implement update and delete event endpoints

--- server/src/domain/users/events/controllers.py
+++ server/src/domain/users/events/controllers.py
@@ -1,16 +1,17 @@
-from litestar import Controller, post
+from litestar import Controller, Response, post, patch, delete
+from litestar.status_codes import HTTP_204_NO_CONTENT
 from litestar.dto import DTOData
 from litestar.di import Provide
 
 from models.user import User
 from models.event import Event
 from domain.users.events.repositories import EventRepository
-from domain.users.events.dependencies import provide_events_repo
-from domain.users.events.dtos import CreateEventDTO, ReadEventDTO
+from domain.users.events.dependencies import provide_events_repo, provide_event
+from domain.users.events.dtos import CreateEventDTO, UpdateEventDTO, ReadEventDTO
 from domain.users.events.validators import validate_event
 
 class EventController(Controller):
-    dependencies = {"events_repo": Provide(provide_events_repo)}
+    dependencies = {"events_repo": Provide(provide_events_repo), "event": Provide(provide_event)}
 
     @post(path="/", dto=CreateEventDTO, return_dto=ReadEventDTO)
     async def create_event(self, data: DTOData[Event], user: User, events_repo: EventRepository) -> Event:
@@ -20,3 +21,15 @@ class EventController(Controller):
         await events_repo.add(event, auto_commit=True)
 
         return event
+
+    @patch(path="/{event_id:str}", dto=UpdateEventDTO)
+    async def update_event(self, data: DTOData[Event], event: Event) -> None:
+        # Update event
+        data.update_instance(event)
+        validate_event(event)
+
+        return Response(content="", status_code=HTTP_204_NO_CONTENT)
+
+    @delete(path="/{event_id:str}")
+    async def remove_event(self, event: Event, events_repo: EventRepository) -> None:
+        await events_repo.delete(event.id)
--- server/src/domain/users/events/dtos.py
+++ server/src/domain/users/events/dtos.py
@@ -5,5 +5,8 @@ from models.event import Event
 class CreateEventDTO(SQLAlchemyDTO[Event]):
     config = SQLAlchemyDTOConfig(exclude={"id", "user_id", "user"})
 
+class UpdateEventDTO(SQLAlchemyDTO[Event]):
+    config = SQLAlchemyDTOConfig(exclude={"id", "user_id", "user"}, partial=True)
+
 class ReadEventDTO(SQLAlchemyDTO[Event]):
     config = SQLAlchemyDTOConfig(exclude={"user_id", "user"}, rename_fields={"id": "event_id"})
