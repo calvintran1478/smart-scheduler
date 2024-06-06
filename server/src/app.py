d1f080c09c5c57f2cf8fbe55845b09e34be3f83e
Calvin Tran <calvintran1478@protonmail.com>
2024-06-06 16:12:05 -0400

    Add CORS middleware

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,5 @@
 from litestar import Litestar
+from config.cors import cors_config
 from config.plugins import plugins
 from middleware.auth import auth_middleware
 from lib.exception import exception_handlers
@@ -7,6 +8,7 @@ from routers.router import router
 app = Litestar(
     route_handlers=[router],
     middleware=[auth_middleware],
+    cors_config=cors_config,
     plugins=plugins,
     exception_handlers=exception_handlers
 )
