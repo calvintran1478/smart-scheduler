d6e3a613d2a210e99280d6c8f36e92b52a479142
Calvin Tran <calvintran1478@protonmail.com>
2024-07-11 01:13:55 -0400

    Optimize AI scheduler using Python generators

--- server/src/lib/constraint.py
+++ server/src/lib/constraint.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from math import floor, ceil
-from copy import deepcopy
-from typing import Optional, Literal
+from typing import Optional, Literal, Generator
 from uuid import UUID
+from itertools import chain
 
 from litestar.status_codes import HTTP_409_CONFLICT
 from litestar.exceptions import ClientException
@@ -15,7 +15,7 @@ FAILURE = "FAILURE"
 
 # Type definitions
 type TimeBlock = tuple[float, float]
-type Domain = list[int]
+type Domain = Generator[int, None, None]
 type Relation = callable[[dict[TimeVariable, int]], bool]
 type Constraint = tuple[list[TimeVariable], Relation]
 type PartialSolution = dict[TimeVariable, int | None]
@@ -41,29 +41,31 @@ class ConstraintSatisfactionProblem:
 def forward_check(csp: ConstraintSatisfactionProblem, assignment: PartialSolution, assigned_var: TimeVariable, value: int, preferred_value_spacing: int) -> Failure | None:
     """Remove domain values which are inconsistent with the given assignment. If a domain becomes empty in this process a failure is returned"""
     # Determine interval numbers directly blocked by assignment
-    intervals_occupied = list(range(value, value + assigned_var.duration)) 
+    intervals_occupied = list(range(value, value + assigned_var.duration))
 
     # Reduce unassigned variable domains based on new assignment
-    unassigned_variables = [var for var in assignment.keys() if assignment[var] == None]  
+    unassigned_variables = (var for var in assignment.keys() if assignment[var] == None)
     for unassigned_var in unassigned_variables:
         # Calculate new domain
-        intervals_to_remove = intervals_occupied + list(range(value - unassigned_var.duration + 1, value))
-        csp.variable_domains[unassigned_var] = [interval for interval in csp.variable_domains[unassigned_var] if interval not in intervals_to_remove]
+        csp.variable_domains[unassigned_var] = (interval for interval in csp.variable_domains[unassigned_var] if interval not in intervals_occupied)
+        csp.variable_domains[unassigned_var] = (interval for interval in csp.variable_domains[unassigned_var] if interval not in range(value - unassigned_var.duration + 1, value))
 
         # Check if domain is empty
-        if (len(csp.variable_domains[unassigned_var]) == 0):
+        domain_lst = list(csp.variable_domains[unassigned_var])
+        if (len(domain_lst) == 0):
             return FAILURE
 
         # Prioritize values away from the new assignment based on the preferred spacing
         first_interval = value
         last_interval = first_interval + assigned_var.duration - 1
-        intervals_to_shift = [interval for interval in range(first_interval - preferred_value_spacing, first_interval)] + [interval for interval in range(last_interval + 1, last_interval + 1 + preferred_value_spacing)]
+        intervals_to_shift = chain(range(first_interval - preferred_value_spacing, first_interval), range(last_interval + 1, last_interval + 1 + preferred_value_spacing))
         for interval in intervals_to_shift:
             try:
-                csp.variable_domains[unassigned_var].remove(interval)
-                csp.variable_domains[unassigned_var].append(interval)
+                domain_lst.remove(interval)
+                domain_lst.append(interval)
             except ValueError:
                 pass
+        csp.variable_domains[unassigned_var] = (interval for interval in domain_lst)
 
 def backtracking_search(csp: ConstraintSatisfactionProblem, preferred_value_spacing: int) -> Optional[Solution]:
     empty_solution = {k: None for k in sorted(csp.variable_domains.keys(), key=lambda var: var.duration, reverse=True)}
@@ -74,11 +76,13 @@ def backtrack(csp: ConstraintSatisfactionProblem, assignment: PartialSolution, p
     if (None not in assignment.values()):
         return assignment
 
-    # Save a copy of the domains to revert back to
-    original_variable_domains = deepcopy(csp.variable_domains)
-
     # Select unassigned variable
     curr_var = next(var for var in assignment.keys() if assignment[var] == None)
+
+    # Save a copy of the domains to revert back to
+    original_variable_domains = {k: list(v) for k, v in csp.variable_domains.items() if assignment[k] == None and k != curr_var}
+    for k, v in original_variable_domains.items():
+        csp.variable_domains[k] = (x for x in v)
 
     # Search for a valid assignment for the selected variable
     for value in csp.variable_domains[curr_var]:
@@ -95,14 +99,14 @@ def backtrack(csp: ConstraintSatisfactionProblem, assignment: PartialSolution, p
                 return result
 
         # Otherwise recover original domains to try a new variable assignment
-        assignment[curr_var] = None
-        csp.variable_domains = deepcopy(original_variable_domains)
+        for k, v in original_variable_domains.items():
+            csp.variable_domains[k] = (x for x in v)
 
 def schedule_work_sessions(time_blocks: list[TimeBlock], schedule_id: UUID, session_durations: list[int], best_focus_times: list[TimeBlock], preferred_break_length: int) -> list[ScheduleItem]:
     # Initialize starting domain
     seconds_per_interval = 60 * 15
     num_intervals = floor(86400 / seconds_per_interval)
-    domain = list(range(num_intervals))
+    domain = range(num_intervals)
     start_intervals = []
     for time_block in time_blocks:
         # Get interval numbers that overlap with the time block
@@ -112,20 +116,21 @@ def schedule_work_sessions(time_blocks: list[TimeBlock], schedule_id: UUID, sess
         interval_numbers = range(first_interval, int(last_interval) + 1)
 
         # Reduce domain
-        domain = [x for x in domain if x not in interval_numbers]
+        domain = (x for x in domain if x not in interval_numbers)
 
         # Track start intervals for variable specific domain reductions
         start_intervals.append(first_interval)
 
     # Prioritize best focus times
+    domain_lst = list(domain)
     for preferred_time_interval in best_focus_times:
         first_best_focus_interval = floor(preferred_time_interval[0] / seconds_per_interval)
         result = preferred_time_interval[1] / seconds_per_interval
         last_best_focus_interval = result - 1 if result.is_integer() else floor(result)
         for interval in range(int(last_best_focus_interval), first_best_focus_interval - 1, -1):
             try:
-                domain.remove(interval)
-                domain.insert(0, interval)
+                domain_lst.remove(interval)
+                domain_lst.insert(0, interval)
             except ValueError:
                 pass
 
@@ -135,10 +140,9 @@ def schedule_work_sessions(time_blocks: list[TimeBlock], schedule_id: UUID, sess
     # Determine domain for each variable
     variable_domains = {}
     for time_variable in time_variables:
-        intervals_to_remove = list(range(num_intervals - time_variable.duration + 1, num_intervals))
+        variable_domains[time_variable] = (x for x in domain_lst if x not in range(num_intervals - time_variable.duration + 1, num_intervals))
         for start_interval in start_intervals:
-            intervals_to_remove += list(range(start_interval - time_variable.duration + 1, start_interval))
-        variable_domains[time_variable] = [interval for interval in domain if interval not in intervals_to_remove]
+            variable_domains[time_variable] = (x for x in variable_domains[time_variable] if x not in range(start_interval - time_variable.duration + 1, start_interval))
 
     # Solve constraint satisfaction problem to produce user work sessions
     csp = ConstraintSatisfactionProblem(variable_domains, [])
