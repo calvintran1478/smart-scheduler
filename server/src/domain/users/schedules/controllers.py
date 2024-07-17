0482663a8b91eb0607d879015a99860c4d103611
Calvin Tran <calvintran1478@protonmail.com>
2024-07-17 18:05:36 -0400

    Implement update schedule item endpoint

--- server/src/domain/users/schedules/controllers.py
+++ server/src/domain/users/schedules/controllers.py
@@ -1,4 +1,6 @@
-from litestar import Controller, get
+from litestar import Controller, get, patch
+from litestar.status_codes import HTTP_204_NO_CONTENT, HTTP_409_CONFLICT
+from litestar.exceptions import ClientException, NotFoundException
 from litestar.di import Provide
 
 from models.schedule import Schedule
@@ -6,6 +8,7 @@ from models.schedule_item import ScheduleItem, ScheduleItemTypeEnum
 from models.user import User
 from domain.users.schedules.repositories import ScheduleRepository
 from domain.users.schedules.dependencies import provide_schedules_repo
+from domain.users.schedules.schemas import UpdateScheduleItemInput
 from domain.users.schedules.dtos import ScheduleDTO
 from domain.users.preferences.repositories import PreferenceRepository
 from domain.users.preferences.dependencies import provide_preferences_repo
@@ -16,6 +19,7 @@ from lib.time import convert_to_utc, seconds_to_time_object
 from lib.schedule import requires_refresh, ScheduleBuilder, ScheduleDirector
 
 from datetime import date, datetime, timedelta
+from uuid import UUID
 
 class ScheduleController(Controller):
     dependencies = {
@@ -52,3 +56,37 @@ class ScheduleController(Controller):
         await schedules_repo.update(schedule, auto_commit=True)
 
         return schedule
+
+    @patch(path="/{schedule_date:date}/schedule_items/{schedule_item_id:uuid}", status_code=HTTP_204_NO_CONTENT)
+    async def update_schedule_item(self, data: UpdateScheduleItemInput, user: User, schedule_date: date, schedule_item_id: UUID, schedules_repo: ScheduleRepository) -> None:
+        # Search for schedule item
+        selected_schedule_item = None
+        schedule = await schedules_repo.get_one_or_none(user_id=user.id, date=schedule_date)
+        if (schedule == None):
+            raise NotFoundException(detail="Schedule item not found")
+
+        try:
+            selected_schedule_item = next(schedule_item for schedule_item in schedule.schedule_items if schedule_item.id == schedule_item_id)
+        except StopIteration:
+            raise NotFoundException(detail="Schedule item not found")
+
+        # Check if this schedule item can be modified
+        if (selected_schedule_item.schedule_item_type in [ScheduleItemTypeEnum.SLEEP, ScheduleItemTypeEnum.EVENT]):
+            raise ClientException(detail="Schedule item must be a habit session or work session")
+
+        # Update time values
+        if (data.start_time != None):
+            selected_schedule_item.start_time = data.start_time
+
+        if (data.end_time != None):
+            selected_schedule_item.end_time = data.end_time
+
+        # Check if new time values are valid
+        if (selected_schedule_item.start_time > selected_schedule_item.end_time):
+            raise ClientException(detail="Start time must come before end time")
+
+        if any(selected_schedule_item.end_time > schedule_item.start_time and selected_schedule_item.start_time < schedule_item.end_time \
+            for schedule_item in schedule.schedule_items if schedule_item.id != selected_schedule_item.id):
+                raise ClientException(detail="New times must not overlap with existing schedule items", status_code=HTTP_409_CONFLICT)
+
+        await schedules_repo.update(schedule, auto_commit=True)
--- server/src/domain/users/schedules/dtos.py
+++ server/src/domain/users/schedules/dtos.py
@@ -5,10 +5,11 @@ from models.schedule import Schedule
 class ScheduleDTO(SQLAlchemyDTO[Schedule]):
     config = SQLAlchemyDTOConfig(
         include={
-            "date",
+            "schedule_items.0.id",
             "schedule_items.0.name",
             "schedule_items.0.start_time",
             "schedule_items.0.end_time",
             "schedule_items.0.schedule_item_type"
-        }
+        },
+        rename_fields={"schedule_items.0.id": "schedule_item_id"}
     )
